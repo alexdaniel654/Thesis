0c90cb1a257d8ac5896f55d2b1049266e0048607
Alex Daniel <alexander.daniel@nottingham.ac.uk>
2021-01-12 12:43:35 +0000

    First draft of theory

--- Images/Theory/TFE/angles.py
+++ Images/Theory/TFE/angles.py
@@ -22,8 +22,8 @@ matplotlib.rcParams.update({
 def sss(alpha, tr, t1):
     return ((np.sin(alpha) * (1 - np.exp(-tr/t1)))/(1 - np.cos(alpha) * np.exp(-tr/t1)))
 
-tr = np.linspace(0, 5000, 100)
-alpha = np.linspace(0, np.pi, 100)
+tr = np.linspace(0, 5000, 500)
+alpha = np.linspace(0, np.pi, 500)
 # Values for renal cortex from Cox et al 2017
 t1 = 1376
 t2star = 49.6
@@ -38,17 +38,16 @@ df['signal'] = sig.reshape(-1)
 df = df.pivot('alpha', 'tr', 'signal')
 
 fig, ax = plt.subplots()
-# cbar = fig.colorbar(df)
-ax = sns.heatmap(df, cbar=False, ax=ax)
-ax.set_xticks(np.linspace(0, 100, 6))
+ax = sns.heatmap(df, cbar=False, ax=ax, cmap='inferno')
+ax.set_xticks(np.linspace(0, 500, 6))
 ax.set_xticklabels(['{:.0f}'.format(x) for x in np.linspace(0, 5000, 6)])
 ax.set_xlabel('TR ($ms$)')
-ax.set_yticks(np.linspace(0, 100, 5))
+ax.set_yticks(np.linspace(0, 500, 5))
 ax.set_yticklabels(['{:.0f}'.format(x) for x in np.linspace(0, 180, 5)])
 ax.set_ylabel('Flip Angle ($^\circ$)')
-cbar = fig.colorbar(plt.imshow(sig))
-cbar.ax.set_ylabel('$S_{TFE}$')
-ax.plot(np.arange(0.5, 100.5), (ernst/180)*100, label='Ernst Angle')
+cbar = fig.colorbar(plt.imshow(sig, cmap='inferno'))
+cbar.ax.set_ylabel('$S_{TFE}$/$M_0$')
+ax.plot(np.arange(0.5, 500.5), (ernst/180)*500, label='Ernst Angle', color='C2')
 ax.legend(loc='lower left')
 
 fig.savefig('ernst.pdf', bbox_inches='tight', dpi=300)
